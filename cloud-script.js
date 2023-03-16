3fc85ba2945f8eef48500c6844daecbab6401559
Linh <128013287+UserKniLCV@users.noreply.github.com>
2023-03-16 11:06:57 +0700

    Update cloud-script.js

--- cloud-script.js
+++ cloud-script.js
@@ -1,45 +1,33 @@
-// Define the Cloud Script function
 handlers.getPlayerDataList = function (args, context) {
     // Set up the API request parameters
     var request = {
         PlayFabIds: null,
-        Keys: ["Player Data"]
+        Keys: ["Coins"]
+    };
+
+    // Set the headers for the API request
+    var headers = {
+        "Content-Type": "application/json",
+        "X-PlayFabSDK-Version": "1.0.0",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type,X-PlayFabSDK-Version",
+        "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS"
     };
 
     // Call the API to get the list of players
-    server.GetAllUsers({}, function (error, result) {
-        if (error) {
-            // Return an error message if the API call failed
-            return { error: error };
-        } else {
-            // Map the PlayFab IDs of the players to an array
-            var playFabIds = result.data.Users.map(function (user) {
-                return user.PlayFabId;
-            });
-
-            // Set the PlayFab IDs for the next API request
-            request.PlayFabIds = playFabIds;
-
-            // Call the API to get the player data
-            server.GetUserData(request, function (error, result) {
-                if (error) {
-                    // Return an error message if the API call failed
-                    return { error: error };
-                } else {
-                    // Create an object to store the player data
-                    var playerDataList = {};
-
-                    // Loop through the player data and add it to the object
-                    for (var playFabId in result.data.Data) {
-                        if (result.data.Data.hasOwnProperty(playFabId)) {
-                            playerDataList[playFabId] = result.data.Data[playFabId]["Player Data"].Value;
-                        }
-                    }
-
-                    // Return the list of player data as JSON
-                    return { playerDataList: playerDataList };
-                }
-            });
+    server.HttpRequest(
+        "https://B6E0C.playfabapi.com/CloudScript/ExecuteFunction",
+        "POST",
+        JSON.stringify({ FunctionName: "getPlayerDataList", FunctionParameter: args }),
+        headers,
+        function (error, result) {
+            if (error) {
+                // Return an error message if the API call failed
+                return { error: error };
+            } else {
+                // Return the response as JSON
+                return JSON.parse(result.response);
+            }
         }
-    });
+    );
 };
